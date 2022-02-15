d1f35e57cae509c2d5ea99a80b7e01751f51c6c5
辻 郁矢 <fumiya@tsujinoMacBook-Air.local>
2022-02-15 20:55:15 +0900

    【レスポンシブ】SERVICEレイアウト修正

--- functions.php
+++ functions.php
@@ -1,2 +1,24 @@
 <?php
-define(‘WP_SCSS_ALWAYS_RECOMPILE’, true);
+define("WP_SCSS_ALWAYS_RECOMPILE", true);
+
+/**
+ * スマートフォンであるか判定する
+ * 
+ */
+function is_mobile(){
+    $useragents = array(
+        'iPad',
+        'iPhone',
+        'iPod',
+        'Android',
+        'dream',
+        'CUPCAKE',
+        'Windows',
+        'blackberry',
+        'webOS',
+        'incognito',
+        'webmate'
+    );
+    $pattern = '/'.implode('|', $useragents).'/i';
+    return preg_match($pattern, $_SERVER['HTTP_USER_AGENT']);
+}
--- parts/service.php
+++ parts/service.php
@@ -7,15 +7,15 @@
       <p>事業内容</p>
     </div>
     <div class="service-contents-box">
-      <div class="service-contents"> <img src="<?php bloginfo('template_directory'); ?>/img/service01.jpg" alt="service01" width="350" height="220" class="service-img" />
+      <div class="service-contents"> <img src="<?php bloginfo('template_directory'); ?>/img/service01.jpg" alt="service01" width="350" height="<?php print wp_is_mobile() ? 210 : 220 ?>" class="service-img" />
         <div class="service-contents-title">Webサイト制作</div>
         <div class="service-contents-text">新規サイトの制作はもちろんサイトリニューアルやランディングページの制作も可能です。</div>
       </div>
-      <div class="service-contents"> <img src="<?php bloginfo('template_directory'); ?>/img/service02.jpg" alt="service02" width="350" height="220" class="service-img" />
+      <div class="service-contents"> <img src="<?php bloginfo('template_directory'); ?>/img/service02.jpg" alt="service02" width="350" height="<?php print wp_is_mobile() ? 210 : 220 ?>" class="service-img" />
         <div class="service-contents-title">Webサイト運用</div>
         <div class="service-contents-text">サイトの更新作業や独自のアクセス解析に基づいたサイト改善のご提案をいたします。</div>
       </div>
-      <div class="service-contents"> <img src="<?php bloginfo('template_directory'); ?>/img/service03.jpg" alt="service013" width="350" height="220" class="service-img" />
+      <div class="service-contents"> <img src="<?php bloginfo('template_directory'); ?>/img/service03.jpg" alt="service013" width="350" height="<?php print wp_is_mobile() ? 210 : 220 ?>" class="service-img" />
         <div class="service-contents-title">アプリ開発</div>
         <div class="service-contents-text">スマートフォンアプリ開発の他、Vue.jsやReactによるWebアプリの開発が可能です。</div>
       </div>
