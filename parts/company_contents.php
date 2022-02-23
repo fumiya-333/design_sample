45d5b7d18a39b11ba635ef8f4d16a607c155a21f
fumiya.tsuji <fumiya0903613@gmail.com>
2022-02-23 23:25:25 +0900

    company画面作成

--- parts/company_contents.php
+++ parts/company_contents.php
@@ -1,6 +1,7 @@
 <?php
 $company_contents_text = "";
 if(wp_is_mobile()){
+  $company_contents_titles = array("サイトのゴール = ", "夢を叶えること");
   $company_contents_text = <<< TEXT
   お客様の夢を叶えること。<br>
   それがWebサイトのゴールであり、<br>
@@ -10,7 +11,42 @@ if(wp_is_mobile()){
   お客様の笑顔を見たい。<br>
   夢を実現する手助けをさせてください。<br>
 TEXT;
+  $company_overview_table = <<< TABLE
+    <table class="company-overview-table">
+      <tr>
+        <th>社名</th>
+      </tr>
+      <tr>
+        <td>株式会社PON DESIGN</td>
+      </tr>
+      <tr>
+        <th>設立</th>
+      </tr>
+      <tr>
+        <td>2020.01.10</td>
+      </tr>
+      <tr>
+        <th>代表取締役</th>
+      </tr>
+      <tr>
+        <td>猫山ポン太郎</td>
+      </tr>
+      <tr>
+        <th>資本金</th>
+      </tr>
+      <tr>
+        <td>1,000,000円</td>
+      </tr>
+      <tr>
+        <th>所在地</th>
+      </tr>
+      <tr>
+        <td>〒555-5555 東京都千代田区 ポンビルディング 606</td>
+      </tr>
+    </table>
+TABLE;
 }else{
+  $company_contents_titles = array("サイトのゴール = 夢を叶えること");
   $company_contents_text = <<< TEXT
   お客様の夢を叶えること。<br>
   それがWebサイトのゴールであり、私たちが目指すことです。<br>
@@ -18,29 +54,7 @@ TEXT;
   お客様の笑顔を見たい。<br>
   夢を実現する手助けをさせてください。
 TEXT;
-}
-?>
-
-<div class="company-contents">
-  <div class="company-message-box">
-    <div class="company-message">
-      <div class="company-message-head">メッセージ</div>
-      <div class="company-message-title">サイトのゴール = 夢を叶えること</div>
-      <div class="company-message-text"><?= $company_contents_text ?></div>
-    </div>
-  </div>
-  <div class="company-president">
-    <div class="company-president-img">
-      <img src="<?php bloginfo('template_directory'); ?>/img/CEO.png" alt="ceo" width="<?php print wp_is_mobile() ? 335 : 445 ?>" height="<?php print wp_is_mobile() ? 248 : 330 ?>"/>
-    </div>
-    <div class="company-president-message">
-      テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります<br><br>
-      テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります<br><br>
-      代表取締役社長 猫山ポン太郎
-    </div>
-  </div>
-  <div class="company-overview">
-    <div class="company-overview-title">会社概要</div>
+  $company_overview_table = <<< TABLE
     <table class="company-overview-table">
       <tr>
         <th>社名</th>
@@ -63,6 +77,37 @@ TEXT;
         <td>〒555-5555 東京都千代田区 ポンビルディング 606</td>
       </tr>
     </table>
+TABLE;
+}
+?>
+
+<div class="company-contents">
+  <div class="company-message-box">
+    <div class="company-message">
+      <div class="company-message-head">メッセージ</div>
+      <div class="company-message-title-box">
+        <?php
+          foreach($company_contents_titles as $company_contents_title){
+            echo '<div class="company-message-title">' . $company_contents_title . '</div>';
+          }
+        ?>
+      </div>
+      <div class="company-message-text"><?= $company_contents_text ?></div>
+    </div>
+  </div>
+  <div class="company-president">
+    <div class="company-president-img">
+      <img src="<?php bloginfo('template_directory'); ?>/img/CEO.png" alt="ceo" width="<?php print wp_is_mobile() ? 335 : 445 ?>" height="<?php print wp_is_mobile() ? 248 : 330 ?>"/>
+    </div>
+    <div class="company-president-message">
+      テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります<br><br>
+      テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります<br><br>
+      代表取締役社長 猫山ポン太郎
+    </div>
+  </div>
+  <div class="company-overview">
+    <div class="company-overview-title">会社概要</div>
+    <?= $company_overview_table ?>
     <div class="company-googlemap">
       <iframe
         src="https://www.google.com/maps?output=embed&z=15&ll=35.6812405,139.7649308&q=東京駅"
